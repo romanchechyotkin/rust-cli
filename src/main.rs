3c04d3bae11dcf49070b6393fbc3dc7f460c70d0
romanchechyotkin <romanchechyotkin@gmail.com>
2024-01-20 21:12:06 +0300

    parsing arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::path;
+
+struct Cli {
+    pattern: String,
+    path: path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let env_args: Vec<String> = env::args().collect();
+
+    let pattern = env_args.get(1).expect("provide pattern");
+    let path = env_args.get(2).expect("provide path");
+
+    println!("pattern={:?}; path={:?};", pattern, path);
+
+    let args = Cli {
+        pattern: pattern.to_string(),
+        path: path::PathBuf::from(path),
+    };
+
+    println!("pattern={:?}; path={:?};", args.pattern, args.path);
 }
